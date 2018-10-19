b2785918503d4ba5fc7016d6738aeac17a8c0691
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-10-19 15:46:16 -0400

    cat map-related files from bedrock into one file, add one example lemma

---
+++ create.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+F=./maps_v0.v
+
+sed -n '/BEGIN LEMMAS/,$p' $F > saved.v
+
+SRC=../bedrock2/compiler/src
+
+FILES="$SRC/../lib/fiat_crypto_tactics/Test.v
+       $SRC/../lib/fiat_crypto_tactics/Not.v
+       $SRC/../lib/LibTacticsMin.v
+       $SRC/Decidable.v
+       $SRC/util/Tactics.v
+       $SRC/util/Set.v
+       $SRC/util/Map.v"
+
+for f in $FILES; do
+    printf '\n(* ** %s *)\n' $f
+    cat $f
+done | sed -E -e 's/Require.* (compiler|lib)\..*/(* \0 *)/g' > $F
+
+printf '\n\n' >> $F
+
+cat saved.v >> $F
+
+rm saved.v
+
+coqc $F
